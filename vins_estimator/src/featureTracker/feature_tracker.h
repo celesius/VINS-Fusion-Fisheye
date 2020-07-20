d532939b0960f4c6bf593b47b27a8481cb63ea57
Xu Hao <xuhao3e8@gmail.com>
2020-07-21 04:02:37 +0800

    Cached

--- vins_estimator/src/featureTracker/feature_tracker.h
+++ vins_estimator/src/featureTracker/feature_tracker.h
@@ -56,6 +56,49 @@ double distance(cv::Point2f pt1, cv::Point2f pt2);
 
 
 template<CvMat>
+class PinholeFeatureTracker {
+//This class is for track points on a pinhole camera
+protected:
+    int base_id = 0;
+    CvMat prev_img;
+    cv::Mat mask;
+    std::vector<cv::Mat> * prev_pyr = nullptr;
+    vector<cv::Point2f> n_pts;
+    vector<cv::Point2f> predict_pts;
+    vector<cv::Point2f> cur_pts;
+    vector<cv::Point2f> cur_un_pts;
+    vector<cv::Point2f> prev_un_pts;
+
+    vector<cv::Point2f> pts_velocity;
+    vector<cv::Point3f> cur_velocity;
+
+    vector<int> ids;
+    map<int, int> pts_status;
+    map<int, int> prevPtsMap;
+    map<int, int> cur_un_pts_map;
+    set<int> removed_pts;
+    vector<int> track_cnt;
+    vector<camodocal::CameraPtr> m_camera;
+
+public:
+    PinholeFeatureTracker() {}
+    virtual void track_image(CvMat img);
+    virtual void track_current_to_right(CvMat img);
+    virtual void setMask() {}
+
+    bool inBorder(const cv::Point2f &pt);
+    bool inBorder(const cv::Point2f &pt, cv::Size shape) const;
+    
+    vector<cv::Point2f> ptsVelocity(vector<int> &ids, vector<cv::Point2f> &pts, 
+        map<int, cv::Point2f> &cur_id_pts, map<int, cv::Point2f> &prev_id_pts);
+
+    vector<cv::Point3f> ptsVelocity3D(vector<int> &ids, vector<cv::Point3f> &pts, 
+        map<int, cv::Point3f> &cur_id_pts, map<int, cv::Point3f> &prev_id_pts);
+
+
+}
+
+template<CvMat>
 class IFeatureFrameTracker {
 
 
@@ -71,14 +114,11 @@ protected:
     virtual FeatureFrame trackImage(double _cur_time, const CvMat &_img, const CvMat &_img1) = 0;
     virtual FeatureFrame trackImage_fisheye(double _cur_time, const std::vector<cv::Mat> & fisheye_imgs_up, const std::vector<cv::Mat> & fisheye_imgs_down) = 0;
 
-    vector<cv::Point2f> ptsVelocity(vector<int> &ids, vector<cv::Point2f> &pts, 
-        map<int, cv::Point2f> &cur_id_pts, map<int, cv::Point2f> &prev_id_pts);
-
-    vector<cv::Point3f> ptsVelocity3D(vector<int> &ids, vector<cv::Point3f> &pts, 
-        map<int, cv::Point3f> &cur_id_pts, map<int, cv::Point3f> &prev_id_pts);
     virtual void setPrediction(map<int, Eigen::Vector3d> &predictPts) {};
     virtual void removeOutliers(set<int> &removePtsIds) {};
 
+    double cur_time;
+    double prev_time;
 
     void detectPoints(const CvMat & img, vector<cv::Point2f> & n_pts, 
         vector<cv::Point2f> & cur_pts, int require_pts, const CvMat & mask = CvMat());
@@ -95,76 +135,23 @@ protected:
 
 
 
-struct Feature {
-
-}
-
-template<CvMat>
-class PinholeFeatureTracker {
-//This class is for track points on a pinhole camera
-protected:
-    int base_id = 0;
-    CvMat prev_img;
-    cv::Mat mask;
-    std::vector<cv::Mat> * prev_pyr = nullptr;
-    vector<cv::Point2f> n_pts;
-    vector<cv::Point2f> predict_pts;
-    vector<cv::Point2f> cur_pts;
-    vector<cv::Point2f> cur_un_pts;
-    vector<cv::Point2f> prev_un_pts;
-    vector<cv::Point2f> pts_velocity;
-    vector<cv::Point3f> cur_velocity;
-    vector<int> ids;
-    map<int, int> pts_status;
-    map<int, int> prevPtsMap;
-    map<int, int> cur_un_pts_map;
-    set<int> removed_pts;
-    vector<int> track_cnt;
-
-public:
-    PinholeFeatureTracker() {}
-    virtual void track_image(CvMat img);
-    virtual void track_current_to_right(CvMat img);
-    virtual void setMask() {}
-
-    bool inBorder(const cv::Point2f &pt);
-    bool inBorder(const cv::Point2f &pt, cv::Size shape) const;
 
 
-}
+// class StereoFeatureTracker : public IFeatureFrameTracker<cv::Mat> {
+//     void addPoints();
+//     void undistortedPoints();
+//     vector<cv::Point2f> undistortedPts(vector<cv::Point2f> &pts, camodocal::CameraPtr cam);
+//     void drawTrack(const cv::Mat &imLeft, const cv::Mat &imRight, 
+//         vector<int> &curLeftIds,
+//         vector<cv::Point2f> &curLeftPts, 
+//         vector<cv::Point2f> &curRightPts,
+//         map<int, cv::Point2f> &prevLeftPtsMap);
+//     void drawTrackImage(cv::Mat & img, vector<cv::Point2f> pts, vector<int> ids, map<int, cv::Point2f> prev_pts);
+//     void setPrediction(map<int, Eigen::Vector3d> &predictPts);
+//     void removeOutliers(set<int> &removePtsIds);
+//     virtual void setPrediction(map<int, Eigen::Vector3d> &predictPts) override;
+//     virtual void removeOutliers(set<int> &removePtsIds) override;
+
+// }
 
 
-class StereoFeatureTracker : public IFeatureFrameTracker<cv::Mat> {
-    void addPoints();
-    void undistortedPoints();
-    vector<cv::Point2f> undistortedPts(vector<cv::Point2f> &pts, camodocal::CameraPtr cam);
-    void drawTrack(const cv::Mat &imLeft, const cv::Mat &imRight, 
-        vector<int> &curLeftIds,
-        vector<cv::Point2f> &curLeftPts, 
-        vector<cv::Point2f> &curRightPts,
-        map<int, cv::Point2f> &prevLeftPtsMap);
-    void drawTrackImage(cv::Mat & img, vector<cv::Point2f> pts, vector<int> ids, map<int, cv::Point2f> prev_pts);
-    void setPrediction(map<int, Eigen::Vector3d> &predictPts);
-    void removeOutliers(set<int> &removePtsIds);
-    virtual void setPrediction(map<int, Eigen::Vector3d> &predictPts) override;
-    virtual void removeOutliers(set<int> &removePtsIds) override;
-
-}
-
-
-class FeatureTracker
-{
-public:
-    
-    map<int, cv::Point3f> cur_up_top_un_pts_map, prev_up_top_un_pts_map;
-    map<int, cv::Point3f> cur_down_top_un_pts_map, prev_down_top_un_pts_map;
-    map<int, cv::Point3f> cur_up_side_un_pts_map, prev_up_side_un_pts_map;
-    map<int, cv::Point3f> cur_down_side_un_pts_map, prev_down_side_un_pts_map;
-
-    map<int, cv::Point2f> prevLeftPtsMap;
-    vector<camodocal::CameraPtr> m_camera;
-    double cur_time;
-    double prev_time;
-    bool stereo_cam;
-    int n_id;
-};
